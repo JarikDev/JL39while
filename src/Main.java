0e120f86025d08de1578e32a3df41b99a2c88986
ivan4000 <sxln1@mail.ru>
2018-10-29 14:14:55 +0300

    JL38doWhile last commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3};
+        int i = 1;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+        while (i < arr.length) {
+            System.out.println(arr[i]);
+            i++;
+        }
+        i=1;
+        do {
+            System.out.println(arr[i]);
+            i++;
+        } while (i ==10);
     }
 }
